b0eb1b269d7dbbf4c8ebaa2d351572bb382f5cbd
lallaWellem <lallawellem@gmail.com>
2023-09-17 14:44:42 -0700

    5again

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,10 +7,6 @@
  */
 int main(void)
 {
-printf("Size of a char: %lu byte(s)\n", sizeof(char));
-printf("Size of an int: %lu byte(s)\n", sizeof(int));
-printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-printf("Size of a float: %lu byte(s)\n", sizeof(float));
-
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
