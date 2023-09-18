3cd35e5355b73c577f5db561804e059c51eb4b8a
lallaWellem <lallawellem@gmail.com>
2023-09-18 10:10:27 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-main.c
+++ 0x04-more_functions_nested_loops/6-main.c
@@ -1,9 +1,10 @@
 #include "main.h"
+
 /**
- * main - check the code 
+ * main - check the code
  * Return: Always 0.
  */
-int main(void);
+int main(void)
 {
 print_line(0);
 print_line(2);
--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * print_line-Draws a strong line according to parameter
+ * print_line - Draws a straight line according to parameter
  * @n: The number of lines to draw
  * Return: empty
  */
 void print_line(int n)
 {
-int x:
+int x;
 if (n <= 0)
 {
 _putchar('\n');
