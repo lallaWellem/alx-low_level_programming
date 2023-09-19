3c6a428681ca2d2ad7122cd87ecbe3208bd0d2a0
lallaWellem <lallawellem@gmail.com>
2023-09-19 01:18:55 -0700

    4print

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * print_rev - Prints a string in reverse
  * @s: The string to print
@@ -8,13 +9,16 @@
 void print_rev(char *s)
 {
 int c = 0;
+
 while (s[c] != '\0')
 {
 c++;
 }
-for (c - = 1; c > = 0; c--)
+
+for (c -= 1; c >= 0; c--)  /* Corrected the syntax here */
 {
 _putchar(s[c]);
 }
+
 _putchar('\n');
 }
