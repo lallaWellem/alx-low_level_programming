8d160bc83f5f452138d7fcdb6f30643019bc116e
lallaWellem <lallawellem@gmail.com>
2023-09-24 03:16:47 -0700

    9times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,20 +6,24 @@
 void times_table(void)
 {
 int x, y, z, u, d;
+    
 for (x = 0; x <= 9; x++)
 {
 for (y = 0; y <= 9; y++)
 {
 z = x * y;
+            
 if (z > 9)
 {
 u = z % 10;
 d = (z - u) / 10;
- if (y != 0)
+                
+if (y != 0)
 {
 _putchar(44);
 _putchar(32);
 }
+                
 _putchar(d + '0');
 _putchar(u + '0');
 }
@@ -31,6 +35,7 @@ _putchar(44);
 _putchar(32);
 _putchar(32);
 }
+                
 _putchar(z + '0');
 }
 }
